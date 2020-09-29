fd761a18cee83188bc9081ae36b38559f0b989c0
Wan Yi <wy524668781@icloud.com>
2020-09-29 20:57:17 +0800

    Initial Commit

---
+++ NUS FYP ARTeachingAssistant/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NUS FYP ARTeachingAssistant
+//
+//  Created by yi 万 on 29/09/2020.
+//  Copyright © 2020 yi 万. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
